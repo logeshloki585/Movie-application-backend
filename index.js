bdce974d36dd6eac0f3084979f317d68ecad452b
logeshloki585 <Logeshtv21@gmail.com>
2023-11-19 00:44:18 +0530

    error correction 4

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-// app.use(cors({credentials:true, origin:"http://localhost:3000"}));
-app.use(cors({credentials:true, origin:"https://movie-application-mauve.vercel.app/"}));
+app.use(cors({credentials:true,  origin: '*'}));
 
 app.use(cookieParser());
 app.use(express.json());
